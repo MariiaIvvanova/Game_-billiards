9878beaa4197499dc123f02b6c20cfabd0ef32d5
MariiaIvvanova <ivanova_mariia36@mail.ru>
2025-02-07 23:21:57 +0300

    enlarged the window

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from copy import deepcopy
 from random import choice
-from config import W, H, GAME_RES, FPS, palette
+from config import W, H, GAME_RES, RES, FPS, palette
 from core.tetromino import figures, Figure
 from core.renderer import draw_grid, draw_figure, draw_field
 from core.input_handler import handle_input
@@ -11,11 +11,12 @@ from core.game import rotate_figure, break_lines, check_borders
 def init_game():
     pygame.init()
     status = "menu"
-    screen = pygame.display.set_mode(GAME_RES)
+    screen = pygame.display.set_mode(RES)
+    game_sc = pygame.Surface(GAME_RES)
     clock = pygame.time.Clock()
     grid = [pygame.Rect(x * 30, y * 30, 30, 30) for x in range(W) for y in range(H)]
     field = [[0 for _ in range(W)] for _ in range(H)]
-    return screen, clock, grid, field, status
+    return screen, game_sc, clock, grid, field, status
 
 
 def process_input(status):
@@ -54,40 +55,59 @@ def update_animation(figure, field, anim_count, anim_speed, anim_limit, f_object
     return figure, color, field, anim_count, anim_limit, status
 
 
-def draw_game(screen, grid, figure, color, field, back_ground, status):
+def draw_game(screen, game_sc, grid, figure, color, field, back_ground, status):
     if status == "to play":
-        screen.fill(pygame.Color('black'))
-        screen.blit(back_ground, (0, 0))
-        draw_grid(screen, grid)
-        draw_figure(screen, figure, pygame.Color(color))
-        draw_field(screen, field)
+        game_sc.blit(back_ground, (0, 0))  # Фон для игрового поля
+        draw_grid(game_sc, grid)
+        draw_figure(game_sc, figure, pygame.Color(color))
+        draw_field(game_sc, field)
+        screen.blit(game_sc, (0, 0))  # Отображение игрового поля на экране
     pygame.display.flip()
 
 
 def main():
-    screen, clock, grid, field, status = init_game()
+    screen, game_sc, clock, grid, field, status = init_game()
     anim_count, anim_speed, anim_limit = 0, 60, 2000
     figure = deepcopy(choice(figures))
     figure = Figure(figure, choice(palette))
+
+    # Загружаем изображения
     menu_screen = pygame.image.load('assets/заставка1.png')
-    menu_screen = pygame.transform.scale(menu_screen, (300, 600))
+    menu_screen = pygame.transform.scale(menu_screen, RES)
+
     game_over_screen = pygame.image.load('assets/game_over.webp')
-    game_over_screen = pygame.transform.scale(game_over_screen, (300, 600))
+    game_over_screen = pygame.transform.scale(game_over_screen, RES)
+
     back_ground = pygame.image.load('assets/рабочий_стол_игры.jpeg')
-    back_ground = pygame.transform.scale(back_ground, (300, 600))
-    screen.blit(menu_screen, (0, 0))
+    back_ground = pygame.transform.scale(back_ground, RES)
+
+    bg = pygame.image.load("assets/bg.jpeg")
+    bg = pygame.transform.scale(bg, RES)
+
     running = True
 
     while running:
         running, dx, rotate, drop_speed, status = process_input(status)
         if drop_speed:
             anim_limit = drop_speed
-        if status == "to play":
+
+        screen.blit(bg, (0, 0))  # Отрисовка основного фона
+
+        if status == "menu":
+            screen.blit(menu_screen, (0, 0))  # Показываем меню
+        elif status == "to play":
+            screen.blit(bg, (0, 0))  # Основной фон
+            game_sc.blit(back_ground, (0, 0))  # Фон для игрового поля
+
             figure.figure = update_figure(figure.figure, dx, rotate, field)
             figure.figure, figure.color, field, anim_count, anim_limit, status = update_animation(figure.figure, field, anim_count, anim_speed, anim_limit, figure)
+
+            draw_game(screen, game_sc, grid, figure.figure, figure.color, field, back_ground, status)  # Отрисовка сетки и фигур
+
         elif status == "game over":
             screen.blit(game_over_screen, (0, 0))
-        draw_game(screen, grid, figure.figure, figure.color,  field, back_ground, status)
+
+        pygame.display.flip()
         clock.tick(FPS)
 
 
